ab902a39f5592ea31c9e28215f2d0855c662e704
iAqsaGhaffar <aqsa.alpeyal98@gmail.com>
2021-01-20 12:54:53 +0500

    Minor updations in Encode funtion

--- AIProject.py
+++ AIProject.py
@@ -22,13 +22,12 @@ private_key = StringVar()
 mode = StringVar()
 Result = StringVar()
 
-###define function#####
-#function to encode
+###define functions#####
 
+#function to encode
 def Encode(key,message):
     enc=[]
     for i in range(len(message)):
         key_c = key[i % len(key)]
         enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
-        
     return base64.urlsafe_b64encode("".join(enc).encode()).decode()
