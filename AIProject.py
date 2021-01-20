a60014bf0fa9af6772f1e7685396f3411c336cb9
iAqsaGhaffar <aqsa.alpeyal98@gmail.com>
2021-01-20 12:08:42 +0500

    Defined the varibles & function to encode

--- AIProject.py
+++ AIProject.py
@@ -14,3 +14,21 @@ root.title("DataFlair - Message Encode and Decode")
 
 Label(root, text ='ENCODE DECODE', font = 'arial 20 bold').pack()
 Label(root, text ='DataFlair', font = 'arial 20 bold').pack(side =BOTTOM)
+
+#define variables
+
+Text = StringVar()
+private_key = StringVar()
+mode = StringVar()
+Result = StringVar()
+
+###define function#####
+#function to encode
+
+def Encode(key,message):
+    enc=[]
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        enc.append(chr((ord(message[i]) + ord(key_c)) % 256))
+        
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
